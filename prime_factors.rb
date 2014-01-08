2b0d733f1c09fc91453ffb4197dec4b36148f137
Apprentice <apprentice@dbc03.local>
2014-01-08 12:48:02 -0800

    Backup of work on prime_factor

--- prime_factors.rb
+++ prime_factors.rb
@@ -7,18 +7,22 @@ def prime_factors(n)
 end
 
 def prime_factors_recursive(n, factors)
-  factors << n if Prime.prime?(n)
-  Prime.each { |prime|
-    if n % prime == 0
-      factors << prime
-      prime_factors(n / prime)
-    end
-  }
+  if Prime.prime?(n)
+    factors << n
+  else
+    Prime.each(1000) { |prime|
+      if n % prime == 0
+        factors << prime
+        prime_factors(n / prime)
+      end
+    }
+  end
+  factors
 end
 
 #-----DRIVERS-----
-p prime_factors(3) == [3]
-p prime_factors(6) == [2,3]
-p prime_factors(8) == [2,2,2]
-p prime_factors(25) == [5,5]
-p prime_factors(123123123) == [3, 3, 41, 333667]
+p prime_factors(3) #== [3]
+p prime_factors(6) #== [2,3]
+p prime_factors(8) #== [2,2,2]
+p prime_factors(25) #== [5,5]
+p prime_factors(147) #== [3,7,7]
