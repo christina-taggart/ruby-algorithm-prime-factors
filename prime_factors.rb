0ad7a28209076271908cf54c52a9d50b37fdf30e
Apprentice <apprentice@dbc03.local>
2014-01-08 13:22:26 -0800

    Adds solution to prime_factors

--- prime_factors.rb
+++ prime_factors.rb
@@ -4,20 +4,24 @@ require 'prime'
 def prime_factors(n)
  factors = []
  prime_factors_recursive(n, factors)
+ factors
 end
 
 def prime_factors_recursive(n, factors)
   if Prime.prime?(n)
     factors << n
   else
-    Prime.each(1000) { |prime|
-      if n % prime == 0
-        factors << prime
-        prime_factors(n / prime)
-      end
-    }
+    prime_factor = first_prime_factor(n)
+    factors << prime_factor
+    prime_factors_recursive(n / prime_factor, factors)
   end
-  factors
+end
+
+# Helper method to find the first prime factor of n:
+def first_prime_factor(n)
+  Prime.each { |prime|
+      return prime if n % prime == 0
+    }
 end
 
 #-----DRIVERS-----
@@ -26,3 +30,4 @@ p prime_factors(6) #== [2,3]
 p prime_factors(8) #== [2,2,2]
 p prime_factors(25) #== [5,5]
 p prime_factors(147) #== [3,7,7]
+p prime_factors(123123123) # == [3, 3, 41, 333667]
