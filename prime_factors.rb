0343e06b38333918d06efa3855c1094782158c35
christina-taggart <tagg.christina@gmail.com>
2014-01-08 17:16:42 -0800

    Update prime_factors.rb
    added examples of refactoring for using the call stack

--- prime_factors.rb
+++ prime_factors.rb
@@ -24,3 +24,45 @@ p prime_factors(8)         # => [2,2,2]
 p prime_factors(25)        # => [5,5]
 p prime_factors(123123123) # => [3, 3, 41, 333667]
 
+
+
+# require 'prime'
+# def prime_factors(n)
+#   prime_factors = []
+#   add_smallest_prime_factor_to_array(n, prime_factors)
+#   return prime_factors
+# end
+
+# def add_smallest_prime_factor_to_array(num)
+#   return [] if num == 1
+#   # find smallest factor
+#   # add it to the prime_factors list
+#   # add the result of the recursive call to the prime_factors list
+#   small_factor = find_smallest_factor(num)
+#   prime_factors += [small_factor] + add_smallest_prime_factor_to_array(num/small_factor)
+# end
+
+# def find_smallest_factor(n)
+#   Prime.each do |prime|
+#     return prime if n%prime == 0
+#   end
+# end
+
+
+
+# def mystery(a, b)
+#   if  b==1
+#     return [a]
+#   else
+#     return [a] + mystery(a, b-1)
+#   end
+# end
+
+# p mystery(10,3)
+
+# # return 10 + ? (mystery(a, 2))
+
+# # return 10 +   10 + ? mystery(a,1)
+# # return 10 + 10 + 10
+
+# # 30
